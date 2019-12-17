044ae927231102a218f27ebdb343ae7bc80116f0
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-12-16 23:30:50 -0800

    adding heroku allowed url

--- SportBook/settings.py
+++ SportBook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o$6!4&$*lkmtq6lubv+1bavq6^u%izmcu&mcfs*o0z9uk18-e7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'sportsbook.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'sportanalyst.herokuapp.com']
 
 
 # Application definition
