e5c462db4d0d3d83e210d8450758ddada1654b93
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2020-01-10 16:38:31 -0500

    added favicon

--- soccer/urls.py
+++ soccer/urls.py
@@ -3,10 +3,15 @@ from soccer.views import HomePageView, DetailPageView, MyAccountPageView, saveMa
 
 app_name = 'soccer'
 urlpatterns = [
+    # Home page link
     path('', HomePageView.as_view(), name='home'),
+    # Detail page link with match id
     path('<int:fixtureId>/', DetailPageView.as_view(), name='detail'),
+    # myaccount link
     path('myaccount/', MyAccountPageView.as_view(), name='myaccount'),
+    # path to save a match
     path('match/<int:fixtureId>/save', saveMatch, name='savematch'),
+    # path to delete a match
     path('match/<int:fixtureId>/delete', deleteMatch, name='deletematch'),
 
 ]
