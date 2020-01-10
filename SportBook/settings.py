37cdfdd3c4e23694a8aa768f373314525c2eeaea
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2020-01-09 23:09:13 -0500

    deploying to heroku

--- SportBook/settings.py
+++ SportBook/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATIC_URL = '/static/'
